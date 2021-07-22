c09cd534b397d9b484140d587c32868ff2e8108a
Matt Farrow <matt@formalizedcuriosity.net>
2021-07-21 20:32:26 -0500

    set up shiny app

---
+++ server.R
@@ -0,0 +1,169 @@
+library(shiny)
+library(arules)
+library(arulesViz)
+library(dplyr)
+library(visNetwork)
+library(igraph)
+
+#data(lab3)
+load(here::here("data", "lab3.Rdata"))
+
+# Convert everything to a factor
+df <- df2 %>%
+  mutate_if(is.ordered, as.factor)
+
+function(input, output) {
+  rules <- reactive({
+    variables <- tibble(variable = colnames(df))
+    
+    #ds <- dataset[, variables$variable]
+    
+    tr <- as(df, 'transactions')
+    
+    arAll <- apriori(
+      tr,
+      parameter = list(
+        support = input$supp,
+        confidence = input$conf,
+        minlen = input$minL,
+        maxlen = input$maxL
+      )
+    )
+    
+    colsL <- variables$variable
+    colsR <- variables$variable
+    
+    if (!is.null(input$lhs)) {
+      colsL <- variables %>%
+        filter(variable %in% input$lhs) %>%
+        select(variable)
+      colsL <- colsL$variable
+    }
+    
+    if (!is.null(input$rhs)) {
+      colsR <- variables %>%
+        filter(category %in% input$rhs) %>%
+        select(variable)
+      colsR <- colsR$variable
+    }
+    
+    varsL <- character()
+    for (i in 1:length(colsL)) {
+      tmp <- colsL[i]
+      if (!is.logical(df[, colsL[i]])) {
+        tmp <-
+          with(df, paste(colsL[i], '=', levels(as.factor(
+            get(colsL[i])
+          )), sep = ''))
+      }
+      varsL <- c(varsL, tmp)
+    }
+    
+    varsR <- character()
+    for (i in 1:length(colsR)) {
+      tmp <- colsR[i]
+      if (!is.logical(df[, colsR[i]])) {
+        tmp <-
+          with(df, paste(colsR[i], '=', levels(as.factor(
+            get(colsR[i])
+          )), sep = ''))
+      }
+      varsR <- c(varsR, tmp)
+    }
+    
+    ar <- subset(arAll, subset = lhs %in% varsL & rhs %in% varsR)
+    
+    quality(ar)$chiSquare <-
+      interestMeasure(ar, measure = 'chiSquare', transactions = tr)
+    quality(ar)$conviction <-
+      interestMeasure(ar, measure = 'conviction', transactions = tr)
+    ar
+  })
+  
+  # Rule length
+  nR <- reactive({
+    nRule <-
+      ifelse(input$samp == 'All Rules', length(rules()), input$nrule)
+  })
+  
+  ## Grouped Plot #########################
+  output$groupedPlot <- renderPlot({
+    ar <- rules()
+    plot(sort(ar, by = input$sort)[1:nR()],
+         method = 'grouped',
+         control = list(k = input$k))
+  }, height = 800, width = 800)
+  
+  ## Graph Plot ##########################
+  output$graphPlot <- renderPlot({
+    ar <- rules()
+    plot(
+      sort(ar, by = input$sort)[1:nR()],
+      method = 'graph',
+      control = list(type = input$graphType)
+    )
+  }, height = 800, width = 800)
+  
+  ## Scatter Plot ##########################
+  output$scatterPlot <- renderPlot({
+    ar <- rules()
+    plot(sort(ar, by = input$sort)[1:nR()], method = 'scatterplot')
+  }, height = 800, width = 800)
+  
+  ## Rules Data Table ##########################
+  output$rulesDataTable <- renderDataTable({
+    rulesdt <- as(rules(), "data.frame")
+    rulesdt
+  })
+  
+  ## Rules Printed ########################
+  output$rulesTable <- renderPrint({
+    ar <- rules()
+    inspect(sort(ar, by = input$sort))
+  })
+  
+  ## Download data to csv ########################
+  output$downloadData <- downloadHandler(
+    filename = 'arules_data.csv',
+    content = function(file) {
+      arules.show <- as(rules(), "data.frame")
+      result <- arules.show$rules
+      result <- gsub('\\{', '', result)
+      result <- gsub('\\}', '', result)
+      result <- strsplit(result, ' => ')
+      result <- as.data.frame(do.call(rbind, result))
+      arules.table <- arules.show %>%
+        mutate(lhs = result$V1,
+               rhs = result$V2) %>%
+        select(lhs,
+               rhs,
+               support,
+               confidence,
+               lift,
+               chiSquare) %>%
+        arrange(desc(chiSquare))
+      write.csv(arules.table, file, row.names = FALSE)
+    }
+  )
+  
+  ## Download report ########################
+  output$downloadReport <- downloadHandler(
+    filename = function() {
+      paste('my-report', sep = '.', 'pdf')
+    },
+    
+    content = function(file) {
+      src <- normalizePath('report.Rmd')
+      
+      # temporarily switch to the temp dir, in case you do not have write
+      # permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'report.Rmd', overwrite = TRUE)
+      
+      library(rmarkdown)
+      out <- render('report.Rmd', pdf_document())
+      file.rename(out, file)
+    }
+  )
+}
---
+++ ui.R
@@ -0,0 +1,120 @@
+library(shiny)
+library(shinythemes)
+library(shinydashboard)
+
+header <- dashboardHeader(title = "Lab 3",
+                          disable = TRUE)
+
+sidebar <- dashboardSidebar(disable = TRUE)
+
+body <- dashboardBody(fluidRow(column(
+  width = 3,
+  box(
+    width = NULL,
+    title = 'Configuration',
+    status = "warning",
+    solidHeader = TRUE,
+    collapsible = TRUE,
+    numericInput("nrule", 'Number of Rules', 15),
+    radioButtons(
+      'samp',
+      label = 'Sample',
+      choices = c('Sample', 'All Rules'),
+      inline = T
+    ),
+    br(),
+    sliderInput(
+      "supp",
+      "Support:",
+      min = 0,
+      max = 1,
+      value = 0.001 ,
+      step = 1 / 10000
+    ),
+    br(),
+    sliderInput(
+      "conf",
+      "Confidence:",
+      min = 0,
+      max = 1,
+      value = 0.8 ,
+      step = 1 / 10000
+    ),
+    br(),
+    selectInput(
+      'sort',
+      label = 'Sorting Criteria:',
+      choices = c('chiSquare', 'lift', 'confidence', 'support')
+    ),
+    br(),
+    br(),
+    numericInput("minL", "Min. items per set:", 2),
+    br(),
+    numericInput("maxL", "Max. items per set::", 3),
+    br(),
+    selectizeInput(
+      'lhs',
+      'LHS',
+      choices = colnames(df),
+      multiple = TRUE,
+      options = list()
+    ),
+    selectizeInput(
+      'rhs',
+      'RHS',
+      choices = colnames(df),
+      multiple = TRUE,
+      options = list()
+    ),
+    downloadButton('downloadData', 'Download Rules as CSV')
+  )
+),
+column(
+  width = 9,
+  box(
+    width = NULL,
+    title = 'Association Rules',
+    status = 'primary',
+    solidHeader = TRUE,
+    collapsible = TRUE,
+    tabsetPanel(
+      tabPanel(
+        title = 'Graph',
+        value = 'graph',
+        plotOutput("graphPlot", width = '100%', height = '100%'),
+        radioButtons(
+          'graphType',
+          label = 'Graph Type',
+          choices = c('items', 'itemsets'),
+          inline = T
+        )
+      ),
+      tabPanel(
+        title = 'Grouped',
+        plotOutput("groupedPlot", width = '100%', height = '100%'),
+        sliderInput(
+          'k',
+          label = 'Choose # of Rule Clusters',
+          min = 1,
+          max = 150,
+          step = 1,
+          value = 15
+        )
+      ),
+      tabPanel(
+        title = 'Scatter',
+        value = 'scatter',
+        plotOutput("scatterPlot", width = '100%', height = '100%')
+      ),
+      tabPanel(
+        title = 'Data Table',
+        value = 'datatable',
+        dataTableOutput("rulesDataTable")
+      )
+    )
+  )
+)))
+
+dashboardPage(header,
+              sidebar,
+              body)
